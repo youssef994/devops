9adc1253d476419aeb7ea64b4f8d5529625489b4
“youssef” <youssefbenslama99422@gmail.com>
2024-10-31 13:34:06 +0100

    changed test files

---
+++ Notification/src/test/java/com/example/notification/NotificationApplicationTest.java
@@ -0,0 +1,18 @@
+package com.example.notification;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class NotificationApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
---
+++ Question-Anwser/src/test/java/com/example/questionanwser/QuestionAnwserApplicationTest.java
@@ -0,0 +1,16 @@
+package com.example.questionanwser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+class QuestionAnwserApplicationTest {
+    @Test
+    void contextLoads() {
+
+    }
+}
